6640d36965479fe28df5ae375e083032e6467682
andreasonny83 <andreasonny83@gmail.com>
2017-05-20 16:51:23 +0100

    refactor: minor refactoring
    minor refactoring and fadeIn animation on app ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from '../environments/environment';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title: string;
-  floatingClass: boolean;
-  version: number;
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      state('show', style({ opacity: 1 })),
 
-  constructor() {
+      transition(':enter', [ // void => *
+        style({ opacity: 0 }),
+        animate('.3s ease-in'),
+      ])
+    ])
+  ]
+})
+export class AppComponent implements OnInit {
+  public title: string;
+  public floatingClass: boolean;
+  public version: number;
+
+  ngOnInit() {
     this.version = environment.version;
     this.title = 'Online Board';
   }
