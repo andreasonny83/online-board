62293da7d04fca13adcc553c50cc45e50bb12aa9
Andrea Sonny <andreasonny83@gmail.com>
2017-05-22 16:48:20 +0100

    fix(footer): UI bug
    An horizontal scrollbar is present on mobile devices whrn the "back" button is present in the header
    panel.
    
    closes #10

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  public version: string;
+  public homepage: string;
+
   constructor() { }
 
-  ngOnInit() {  }
+  ngOnInit() {
+    this.version = environment.version;
+    this.homepage = environment.homepage;
+  }
 }
